321750103d0130f3963a66a45a3f255ec7f1acd9
iAmRamiro <mercadoeze09@gmail.com>
2023-10-03 12:11:20 -0300

    mas errores!

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,26 +1,4 @@
-import { manager } from "/managers/productManager.js";
+const socketClient = io();
 
 const form = document.getElementById("form");
 const boton = document.getElementById("addProduct");
-
-const addProduct = async () => {
-  const inputTitle = document.getElementById("title");
-  const inputPrice = document.getElementById("price");
-  const inputStock = document.getElementById("stock");
-  const inputCategory = document.getElementById("category");
-  const inputCode = document.getElementById("code");
-  const inputThumbnails = document.getElementById("thumbnails");
-
-  const data = {
-    inputTitle,
-    inputPrice,
-    inputStock,
-    inputCategory,
-    inputCode,
-    inputThumbnails,
-  };
-
-  await manager.addProduct(data);
-};
-
-boton.addEventListener("click", addProduct);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import { __dirname } from "./dirname.js";
-import { join } from "path";
+
 import homeRouter from "./routes/home.router.js";
 import productRouter from "./routes/products.router.js";
 import { Server } from "socket.io";
+
+import { manager } from "./utils/managers/productManager.js";
 
 const app = express();
 
@@ -15,14 +17,15 @@ app.use(express.static(__dirname + "/public"));
 //---------------------------------------------------//
 app.engine("handlebars", engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.set("views", join(__dirname, "views"));
+app.set("views", __dirname + "/views");
 
 //---------------------------------------------------//
 app.use("/products", productRouter);
 app.use("/", homeRouter);
 
-app.listen(8080, () => {
+const httpServer = app.listen(8080, () => {
   console.log("escuchando puerto 8080");
 });
 
-// const socketServer = new Server(httpServer);
+// generar socket del lado del servidor
+const socketServer = new Server(httpServer);
